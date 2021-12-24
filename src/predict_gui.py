5957a4a37b9db9f4572aaccef06f9f26112545d4
YangMH <fastyangmh@gmail.com>
2021-12-24 20:35:21 +0800

    modify the dimension order. add some comments.

--- src/predict_gui.py
+++ src/predict_gui.py
@@ -48,6 +48,7 @@ class PredictGUI(BasePredictGUI):
     def display(self):
         image = self.loader(self.filepath).convert(self.color_space)
         resized_image = self.resize_image(image=image)
+        # set the cmap as gray if resized_image doesn't exist channel
         cmap = 'gray' if len(np.array(resized_image).shape) == 2 else None
         rows, cols = 1, 1
         for idx in range(1, rows * cols + 1):
@@ -66,25 +67,30 @@ class PredictGUI(BasePredictGUI):
         sample = self.transform(sample)
         # the sample dimension is (in_chans, width, height)
         sample = sample.cpu().data.numpy()
+        # convert the sample dimension to (width, height, in_chans)
+        sample = sample.transpose(1, 2, 0)
         # the fake_sample dimension is (1, in_chans, width, height),
         # so use 0 index to get the first fake_sample
         fake_sample = fake_sample[0]
+        # convert the fake_sample dimension to (width, height, in_chans)
+        fake_sample = fake_sample.transpose(1, 2, 0)
         diff = np.abs(sample - fake_sample)
-        cmap = 'gray' if sample.shape[0] == 1 else None
-        rows, cols = len(sample), 3
+        # set the cmap as gray if the sample only has 1 channel
+        cmap = 'gray' if sample.shape[2] == 1 else None
+        rows, cols = 1, 3
         title = ['real', 'fake', 'diff']
         for idx in range(1, rows * cols + 1):
             subplot = self.image_canvas.figure.add_subplot(rows, cols, idx)
             subplot.title.set_text('{}'.format(title[(idx - 1) % 3]))
             if (idx - 1) % 3 == 0:
                 # plot real
-                subplot.imshow(sample[(idx - 1) // cols], cmap=cmap)
+                subplot.imshow(sample, cmap=cmap)
             elif (idx - 1) % 3 == 1:
                 # plot fake
-                subplot.imshow(fake_sample[(idx - 1) // cols], cmap=cmap)
+                subplot.imshow(fake_sample, cmap=cmap)
             elif (idx - 1) % 3 == 2:
                 # plot diff
-                subplot.imshow(diff[(idx - 1) // cols], cmap=cmap)
+                subplot.imshow(diff, cmap=cmap)
             subplot.axis('off')
         self.image_canvas.draw()
 
